6fc8013db759471169a2bf602c0c93a05819d2a2
Htu <lehuutu1207@gmail.com>
2024-11-25 11:04:08 +0700

    Initial commit

---
+++ WebApplication1/Controllers/TrangChuController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        // GET: TrangChu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView("_NavPartial");
+        }
+
+        public ActionResult FooterPartial()
+        {
+            return PartialView("_FooterPartial");
+        }
+    }
+}
