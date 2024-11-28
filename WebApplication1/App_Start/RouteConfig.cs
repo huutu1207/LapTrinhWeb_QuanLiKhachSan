72e5d094c711041263b7f7df771d611636fd1aab
Htu <lehuutu1207@gmail.com>
2024-11-28 20:41:06 +0700

    FixBugPath

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace WebApplication1
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                namespaces: new[] {"QuanLiKhachSan.Controllers"}
+                namespaces: new[] {"WebApplication1.Controllers"}
             );
         }
     }
