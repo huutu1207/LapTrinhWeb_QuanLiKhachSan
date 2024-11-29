43f253d49dff442a7f74a42401f036cabaf8de23
Htu <lehuutu1207@gmail.com>
2024-11-29 11:59:11 +0700

    ChiTietPhhong

--- WebApplication1/Controllers/PhongController.cs
+++ WebApplication1/Controllers/PhongController.cs
@@ -21,9 +21,65 @@ namespace WebApplication1.Controllers
         public ActionResult DanhSachPhong(int ?page)
         {
             var listPhong = LayPhong(20);
-            int iSize = 2;
+            int iSize = 9;
             int iPageNumber = (page ?? 1);
             return View(listPhong.ToPagedList(iPageNumber, iSize));
+        }
+
+        public ActionResult ChiTietPhong(string id)
+        {
+            var phong = db.PHONGs.FirstOrDefault(s => s.MaPH == id);
+            if (phong == null)
+            {
+                return HttpNotFound(); // Nếu không tìm thấy sách, trả về lỗi 404
+            }
+
+            // Lấy danh sách bình luận cùng tên khách hàng
+            var binhLuans = (from bl in db.BINHLUANs
+                             join kh in db.KHACHHANGs on bl.MaKH equals kh.MaKH
+                             where bl.MaPH == id
+                             select new CMMD
+                             {
+                                 NoiDung = bl.NDBL,
+                                 DanhGia = (int)bl.DanhGia,
+                                 ThoiGian = (DateTime)bl.ThoiGian,
+                                 HoTenKhachHang = kh.HoTen
+                             }).ToList();
+
+            ViewBag.BinhLuans = binhLuans;
+            return View(phong);
+
+        }
+
+        [HttpPost]
+        public ActionResult ThemBinhLuan(string maPhong, string NoiDung, int DanhGia)
+        {
+            // Kiểm tra xem người dùng đã đăng nhập chưa
+            var khachHang = (KHACHHANG)Session["User"];
+
+            if (khachHang == null)
+            {
+                // Nếu chưa đăng nhập, lưu URL hiện tại vào Session và điều hướng đến trang đăng nhập
+                Session["ReturnUrl"] = Request.Url.ToString();
+                return RedirectToAction("DangNhap", "User");
+            }
+
+            if (ModelState.IsValid)
+            {
+                BINHLUAN binhLuanMoi = new BINHLUAN
+                {
+                    MaPH = maPhong,
+                    MaKH = khachHang.MaKH,
+                    NDBL = NoiDung,
+                    DanhGia = DanhGia,
+                    ThoiGian = DateTime.Now
+                };
+
+                db.BINHLUANs.Add(binhLuanMoi);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ChiTietSach", new { id = maPhong});
         }
     }
 }
---
+++ WebApplication1/Models/CMMD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CMMD
+    {
+        public string NoiDung { get; set; }
+        public int DanhGia { get; set; }
+        public DateTime ThoiGian { get; set; }
+        public string HoTenKhachHang
+        {
+            get; set;
+        }
+    }
+}
