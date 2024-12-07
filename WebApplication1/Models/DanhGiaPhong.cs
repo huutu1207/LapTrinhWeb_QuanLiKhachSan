f686c629c8c02d835926a7eba14369cdca69c039
Htu <lehuutu1207@gmail.com>
2024-12-07 07:58:35 +0700

    Bay lỗi + phòng tiêu biểu

---
+++ WebApplication1/Models/DanhGiaPhong.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class DanhGiaPhong
+    {
+        public string Name { get; set; }
+        public double rating { get; set; }
+        public double gia { get; set; }
+        public string anh { get; set; }
+        public int cnt { get; set; }
+
+        public string maphong
+        {
+            get; set;
+        }
+    }
+}
